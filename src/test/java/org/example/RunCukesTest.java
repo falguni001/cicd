3fcbd544ada4b516dc628581faca55e1e9e47a8e
Nareshkhunt <nareshkhunt@yahoo.co.in>
2021-12-30 16:43:28 +0000

    reporting

--- src/test/java/org/example/RunCukesTest.java
+++ src/test/java/org/example/RunCukesTest.java
@@ -12,5 +12,16 @@ import org.junit.runner.RunWith;
         tags = "@smoke",
         plugin = "json:target/cucumber-report/cucumber.json")
 public class RunCukesTest {
-
+// a json report is generated as target/cucumber-report
+    //open maven tab and click 'execute maven goal'
+    //enter 'cluecumber-report:reporting' and click ok to build
+    //report will generated in target folder as generated-report
+    //to run from intellij
+    //copy this code in edit configuration
+    //-ea
+    //-Durl="https://www.argos.co.uk"
+    //-Dinstance="chrome"
+    //-Dcucumber.options="--tags @nk"
+    //to run from maven-execute maven goal and from terminal
+    // mvn -Durl="https://www.argos.co.uk" -Dinstance=chrome -Dcucumber.options="--tags @nk" test
 }
